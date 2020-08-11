e880df41a8882a17686aef64c19cd975d95f8a36
Fraser Elliot Carter Smith <fraser.carter@gmail.com>
2020-08-11 14:43:16 +1000

    fixed a bunch of things

--- aws.tf
+++ aws.tf
@@ -6,16 +6,22 @@ locals {
   aws_vpc_id         = "vpc-d0b506b5"
 }
 
-
-resource "aws_customer_gateway" "cgw-gcp-au" {
+resource "aws_customer_gateway" "cgw_gcp_au_01" {
   bgp_asn    = 65200
-  ip_address = "10.61.7.250"
-  type       = "ipsec.1"
+  ip_address = google_compute_address.gcp_vpn_ip.address
+
+  type = "ipsec.1"
+  tags = {
+    "Name" = "cgw-gcp-au-01"
+  }
 }
 
-resource "aws_vpn_connection" "vpn-gcp-au-01" {
-  customer_gateway_id = aws_customer_gateway.cgw-gcp-au.id
+resource "aws_vpn_connection" "vpn_gcp_au_01" {
+  customer_gateway_id = aws_customer_gateway.cgw_gcp_au_01.id
   transit_gateway_id  = local.transit_gateway_id
   type                = "ipsec.1"
   static_routes_only  = false
+  tags = {
+    "Name" = "vpn-gcp-au-01"
+  }
 }
--- gcp.tf
+++ gcp.tf
@@ -29,97 +29,110 @@ locals {
  * ----------Cloud Router----------
  */
 
-resource "google_compute_router" "router-a" {
-  name    = "router-a"
+resource "google_compute_router" "core" {
+  name    = "core"
   region  = var.global["gcp_default_region"]
   network = var.global["gcp_network"]
   bgp {
-    asn = aws_customer_gateway.cgw-gcp-au.bgp_asn
+    asn = var.GCP_ASN
   }
 }
 
-resource "google_compute_router_peer" "router-a-peer" {
-  name            = "gcp-to-aws-bgp-a"
-  router          = google_compute_router.router-a.name
-  region          = google_compute_router.router-a.region
-  peer_ip_address = aws_vpn_connection.vpn-gcp-au-01.tunnel1_vgw_inside_address
-  peer_asn        = var.GCP_TUN1_VPN_GW_ASN
-  interface       = google_compute_router_interface.router_interface1.name
+resource "google_compute_router_peer" "tunnel_01" {
+  name                      = "vpn-aws-au-tunnel-01"
+  router                    = google_compute_router.core.name
+  region                    = google_compute_router.core.region
+  peer_ip_address           = aws_vpn_connection.vpn_gcp_au_01.tunnel1_vgw_inside_address
+  peer_asn                  = var.AWS_TUNNEL_ASN
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.a.name
+
 }
 
-resource "google_compute_router_interface" "router_interface1" {
+resource "google_compute_router_peer" "tunnel_02" {
+  name                      = "vpn-aws-au-tunnel-02"
+  router                    = google_compute_router.core.name
+  region                    = google_compute_router.core.region
+  peer_ip_address           = aws_vpn_connection.vpn_gcp_au_01.tunnel2_vgw_inside_address
+  peer_asn                  = var.AWS_TUNNEL_ASN
+  advertised_route_priority = 200
+  interface                 = google_compute_router_interface.b.name
+
+}
+
+resource "google_compute_router_interface" "a" {
   name       = "gcp-to-aws-interface-a"
-  router     = google_compute_router.router-a.name
-  region     = google_compute_router.router-a.region
-  ip_range   = "${aws_vpn_connection.vpn-gcp-au-01.tunnel1_cgw_inside_address}/${var.GCP_TUN1_CUSTOMER_GW_INSIDE_NETWORK_CIDR}"
-  vpn_tunnel = google_compute_vpn_tunnel.vpn-aws-au-a.name
+  router     = google_compute_router.core.name
+  region     = google_compute_router.core.region
+  ip_range   = "${aws_vpn_connection.vpn_gcp_au_01.tunnel1_cgw_inside_address}/${var.AWS_VPN_CIDR}"
+  vpn_tunnel = google_compute_vpn_tunnel.vpn_aws_au_01.name
 }
 
-resource "google_compute_router" "router-b" {
-  name    = "router-b"
-  region  = var.global["gcp_default_region"]
-  network = var.global["gcp_network"]
-  bgp {
-    asn = aws_customer_gateway.cgw-gcp-au.bgp_asn
-  }
-}
-
-resource "google_compute_router_peer" "router-b-peer" {
-  name            = "gcp-to-aws-bgp-b"
-  router          = google_compute_router.router-b.name
-  region          = google_compute_router.router-b.region
-  peer_ip_address = aws_vpn_connection.vpn-gcp-au-01.tunnel2_vgw_inside_address
-  peer_asn        = var.GCP_TUN2_VPN_GW_ASN
-  interface       = google_compute_router_interface.router_interface2.name
-}
-
-resource "google_compute_router_interface" "router_interface2" {
+resource "google_compute_router_interface" "b" {
   name       = "gcp-to-aws-interface-b"
-  router     = google_compute_router.router-b.name
-  region     = google_compute_router.router-b.region
-  ip_range   = "${aws_vpn_connection.vpn-gcp-au-01.tunnel2_cgw_inside_address}/${var.GCP_TUN2_CUSTOMER_GW_INSIDE_NETWORK_CIDR}"
-  vpn_tunnel = google_compute_vpn_tunnel.vpn-aws-au-b.name
+  router     = google_compute_router.core.name
+  region     = google_compute_router.core.region
+  ip_range   = "${aws_vpn_connection.vpn_gcp_au_01.tunnel2_cgw_inside_address}/${var.AWS_VPN_CIDR}"
+  vpn_tunnel = google_compute_vpn_tunnel.vpn_aws_au_02.name
 }
 
 /*
- * ----------VPN Tunnel1----------
+ * ----------VPN Tunnel----------
  */
 
-resource "google_compute_vpn_tunnel" "vpn-aws-au-a" {
-  name          = "vpn-aws-au-a"
-  peer_ip       = aws_vpn_connection.vpn-gcp-au-01.tunnel1_address
-  shared_secret = aws_vpn_connection.vpn-gcp-au-01.tunnel1_preshared_key
-  ike_version   = 1
-
-  target_vpn_gateway = google_compute_vpn_gateway.vgw-aws-au.self_link
-
-  router = google_compute_router.router-a.name
-
-  depends_on = [
-    google_compute_forwarding_rule.fr_esp,
-    google_compute_forwarding_rule.fr_udp500,
-    google_compute_forwarding_rule.fr_udp4500,
-  ]
+resource "google_compute_address" "gcp_vpn_ip" {
+  name   = "gcp-vpn-ip"
+  region = var.global["gcp_default_region"]
 }
 
-/*
- * ----------VPN Tunnel2----------
- */
+resource "google_compute_forwarding_rule" "fr_esp" {
+  name        = "fr-esp"
+  ip_protocol = "ESP"
+  ip_address  = google_compute_address.gcp_vpn_ip.address
+  target      = google_compute_vpn_gateway.vgw_aws_au.self_link
+}
 
-resource "google_compute_vpn_tunnel" "vpn-aws-au-b" {
-  name          = "vpn-aws-au-b"
-  peer_ip       = aws_vpn_connection.vpn-gcp-au-01.tunnel2_address
-  shared_secret = aws_vpn_connection.vpn-gcp-au-01.tunnel2_preshared_key
+resource "google_compute_forwarding_rule" "fr_udp500" {
+  name        = "fr-udp500"
+  ip_protocol = "UDP"
+  port_range  = "500-500"
+  ip_address  = google_compute_address.gcp_vpn_ip.address
+  target      = google_compute_vpn_gateway.vgw_aws_au.self_link
+}
+
+resource "google_compute_forwarding_rule" "fr_udp4500" {
+  name        = "fr-udp4500"
+  ip_protocol = "UDP"
+  port_range  = "4500-4500"
+  ip_address  = google_compute_address.gcp_vpn_ip.address
+  target      = google_compute_vpn_gateway.vgw_aws_au.self_link
+}
+
+resource "google_compute_vpn_gateway" "vgw_aws_au" {
+  name    = "vgw-aws-au-ip"
+  network = var.global["gcp_network"]
+}
+
+resource "google_compute_vpn_tunnel" "vpn_aws_au_01" {
+  name          = "vpn-aws-au-01"
+  peer_ip       = aws_vpn_connection.vpn_gcp_au_01.tunnel1_address
+  shared_secret = aws_vpn_connection.vpn_gcp_au_01.tunnel1_preshared_key
   ike_version   = 1
 
-  target_vpn_gateway = google_compute_vpn_gateway.vgw-aws-au.self_link
+  target_vpn_gateway = google_compute_vpn_gateway.vgw_aws_au.self_link
 
-  router = google_compute_router.router-b.name
+  router = google_compute_router.core.name
+}
 
-  depends_on = [
-    google_compute_forwarding_rule.fr_esp,
-    google_compute_forwarding_rule.fr_udp500,
-    google_compute_forwarding_rule.fr_udp4500,
-  ]
+resource "google_compute_vpn_tunnel" "vpn_aws_au_02" {
+  name          = "vpn-aws-au-02"
+  peer_ip       = aws_vpn_connection.vpn_gcp_au_01.tunnel2_address
+  shared_secret = aws_vpn_connection.vpn_gcp_au_01.tunnel2_preshared_key
+  ike_version   = 1
+
+  target_vpn_gateway = google_compute_vpn_gateway.vgw_aws_au.self_link
+
+  router = google_compute_router.core.name
+
 }
 
