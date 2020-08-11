c24e1d0dc2f4cd58b97905e05cc58669d6c21fba
Fraser Elliot Carter Smith <fraser.carter@gmail.com>
2020-08-11 10:16:00 +1000

    fixed some syntax errors

--- gcp.tf
+++ gcp.tf
@@ -107,8 +107,8 @@ resource "google_compute_forwarding_rule" "fr_udp4500" {
  * ----------Cloud Router----------
  */
 
-resource "google_compute_router" "gcp-router1" {
-  name    = "gcp-router1"
+resource "google_compute_router" "router-a" {
+  name    = "router-a"
   region  = var.global["gcp_default_region"]
   network = var.global["gcp_network"]
   bgp {
@@ -116,25 +116,25 @@ resource "google_compute_router" "gcp-router1" {
   }
 }
 
-resource "google_compute_router_peer" "gcp-router1-peer" {
-  name            = "gcp-to-aws-bgp1"
-  router          = google_compute_router.gcp-router1.name
-  region          = google_compute_router.gcp-router1.region
+resource "google_compute_router_peer" "router-a-peer" {
+  name            = "gcp-to-aws-bgp-a"
+  router          = google_compute_router.router-a.name
+  region          = google_compute_router.router-a.region
   peer_ip_address = aws_vpn_connection.vpn-gcp-au-01.tunnel1_vgw_inside_address
   peer_asn        = var.GCP_TUN1_VPN_GW_ASN
   interface       = google_compute_router_interface.router_interface1.name
 }
 
 resource "google_compute_router_interface" "router_interface1" {
-  name       = "gcp-to-aws-interface1"
-  router     = google_compute_router.gcp-router1.name
-  region     = google_compute_router.gcp-router1.region
+  name       = "gcp-to-aws-interface-a"
+  router     = google_compute_router.router-a.name
+  region     = google_compute_router.router-a.region
   ip_range   = "${aws_vpn_connection.vpn-gcp-au-01.tunnel1_cgw_inside_address}/${var.GCP_TUN1_CUSTOMER_GW_INSIDE_NETWORK_CIDR}"
   vpn_tunnel = google_compute_vpn_tunnel.vpn-aws-au-a.name
 }
 
-resource "google_compute_router" "gcp-router2" {
-  name    = "gcp-router2"
+resource "google_compute_router" "router-b" {
+  name    = "router-b"
   region  = var.global["gcp_default_region"]
   network = var.global["gcp_network"]
   bgp {
@@ -142,19 +142,19 @@ resource "google_compute_router" "gcp-router2" {
   }
 }
 
-resource "google_compute_router_peer" "gcp-router2-peer" {
-  name            = "gcp-to-aws-bgp2"
-  router          = google_compute_router.gcp-router2.name
-  region          = google_compute_router.gcp-router2.region
+resource "google_compute_router_peer" "router-b-peer" {
+  name            = "gcp-to-aws-bgp-b"
+  router          = google_compute_router.router-b.name
+  region          = google_compute_router.router-b.region
   peer_ip_address = aws_vpn_connection.vpn-gcp-au-01.tunnel2_vgw_inside_address
   peer_asn        = var.GCP_TUN2_VPN_GW_ASN
   interface       = google_compute_router_interface.router_interface2.name
 }
 
 resource "google_compute_router_interface" "router_interface2" {
-  name       = "gcp-to-aws-interface2"
-  router     = google_compute_router.gcp-router2.name
-  region     = google_compute_router.gcp-router2.region
+  name       = "gcp-to-aws-interface-b"
+  router     = google_compute_router.router-b.name
+  region     = google_compute_router.router-b.region
   ip_range   = "${aws_vpn_connection.vpn-gcp-au-01.tunnel2_cgw_inside_address}/${var.GCP_TUN2_CUSTOMER_GW_INSIDE_NETWORK_CIDR}"
   vpn_tunnel = google_compute_vpn_tunnel.vpn-aws-au-b.name
 }
@@ -171,7 +171,7 @@ resource "google_compute_vpn_tunnel" "vpn-aws-au-a" {
 
   target_vpn_gateway = google_compute_vpn_gateway.vgw-aws-au.self_link
 
-  router = google_compute_router.gcp-router1.name
+  router = google_compute_router.router-a.name
 
   depends_on = [
     google_compute_forwarding_rule.fr_esp,
@@ -192,7 +192,7 @@ resource "google_compute_vpn_tunnel" "vpn-aws-au-b" {
 
   target_vpn_gateway = google_compute_vpn_gateway.vgw-aws-au.self_link
 
-  router = google_compute_router.gcp-router2.name
+  router = google_compute_router.router-b.name
 
   depends_on = [
     google_compute_forwarding_rule.fr_esp,
