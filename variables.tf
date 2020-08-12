ced7c68d1a3fb565faf74493c18ef3a0085d8521
Fraser Elliot Carter Smith <fraser.carter@gmail.com>
2020-08-12 11:17:51 +1000

    updated variable

--- variables.tf
+++ variables.tf
@@ -7,9 +7,7 @@ variable "global" {
     "gcp_default_billing_account" = "01B3C9-539FEB-11B3F1"
     "gcp_default_region"          = "australia-southeast1"
     "gcp_default_resource_zone"   = "australia-southeast1-b"
-
     "aws_default_region" = "ap-southeast-2"
-    "gcp_network"        = "gcp-ofx-vpchost-management-vpc"
   }
 }
 
