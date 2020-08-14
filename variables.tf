9f118d2e6dd4e64bc637ef724e9ef042c43f5589
Fraser Elliot Carter Smith <fraser.carter@gmail.com>
2020-08-15 08:14:30 +1000

    removed unnecesary variables from main

--- variables.tf
+++ variables.tf
@@ -10,28 +10,3 @@ variable "global" {
     "aws_default_region"          = "ap-southeast-2"
   }
 }
-
-variable "aws_bgp_asn" {
-  description = "ASN of the AWS Router"
-  default     = "65100"
-}
-
-variable "gcp_bgp_asn" {
-  description = "ASN of the GCP Router"
-  default     = "65200"
-}
-
-variable "gcp_network" {
-  description = "VPC to deploy Cloud Router and HA VPN"
-  default     = ""
-}
-
-variable "transit_gateway_id" {
-  description = "Transit Gateway ID"
-  default     = "tgw-067fc30b039641df1"
-}
-
-variable "aws_network" {
-  description = "VPC to deploy the Customer Gateway and VPN"
-  default     = "vpc-d0b506b5"
-}        
